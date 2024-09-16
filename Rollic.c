d6585c05a515a67e8991b98b3ae84e86e1f710df
Mohammed Ayaan ur Rahman <135800053+ayaan9618@users.noreply.github.com>
2024-09-16 21:10:43 +0530

    clean code

--- Rollic.c
+++ Rollic.c
@@ -31,16 +31,6 @@ int main() {
 
   random(num_rolls);
 
-  /*// Generate the specified number of random dice rolls 
-
-  for (i = 0; i < num_rolls; i++) { 
-// Generates a random number between 1 and 6
-    int roll = (rand() % 6) + 1; 
- 
-
-    printf("Roll %d: %d\n", i+1, roll); 
-
-  } */
 
   return 0; 
 
